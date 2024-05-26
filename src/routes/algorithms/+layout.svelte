67a2cd30fe28ad701f2c71c342883a4f81d88ba9
elkalt <elliott.kalt@gmail.com>
2024-05-25 20:07:26 -0400

    remove header on mobile

--- src/routes/algorithms/+layout.svelte
+++ src/routes/algorithms/+layout.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import { onMount, onDestroy } from 'svelte';
 
+  let isMobile: boolean = false;
+  const handleResize = () => isMobile = window.innerWidth < 768;
+
   let headings: Element[] = [];
   let headerHeight: number;
   let top: number;
@@ -9,7 +12,8 @@
   let headingContainer: HTMLElement;
   let lastScrollY: number = 0;
 
-  const handleScroll = () => {
+  function handleScroll() {
+    if (isMobile) return;
     const rect = headingContainer.getBoundingClientRect();
     const currentScrollY = window.scrollY;
 
@@ -22,35 +26,42 @@
     }
 
     lastScrollY = currentScrollY;
-  };
+  }
 
   onMount(() => {
     if (typeof window !== 'undefined') {
+      handleResize();
+
       headings = Array.from(document.querySelectorAll('h1, h2, h3')).slice(1);
       headerHeight = document.querySelector('header')!.getBoundingClientRect().height;
       top = headerHeight + 24;
       originalHeight = top;
+
       window.addEventListener('scroll', handleScroll);
+      window.addEventListener('resize', handleResize);
     }
   });
 
   onDestroy(() => {
     if (typeof window !== 'undefined') {
       window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('resize', handleResize);
     }
   });
 </script>
 
 <div bind:this={headingContainer} class='heading-container' style:top={`${top}px`} style:position={position}>
-  {#each headings as heading}
-    {@const level = parseInt(heading.tagName.charAt(1))}
-    <a
-      href={`#${heading.id}`}
-      style:margin-left={level - 0.5 + 'rem'}
-      style:border-left={level == 1 ? 'none' : '1px solid var(--tertiary)'}>
-      {heading.textContent} 
-    </a>
-  {/each}
+  {#if !isMobile}
+    {#each headings as heading}
+      {@const level = parseInt(heading.tagName.charAt(1))}
+      <a
+        href={`#${heading.id}`}
+        style:margin-left={level - 0.5 + 'rem'}
+        style:border-left={level == 1 ? 'none' : '1px solid var(--tertiary)'}>
+        {heading.textContent} 
+      </a>
+    {/each}
+  {/if}
 </div>
 <slot />
 
